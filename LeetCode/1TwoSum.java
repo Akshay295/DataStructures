d822030696cb79915f436f54f0be8df4c4531242
AkshayKatole <kshitijakokate14@gmail.com>
2021-05-29 20:02:24 +0530

    username change

--- LeetCode/1TwoSum.java
+++ LeetCode/1TwoSum.java
@@ -2,6 +2,9 @@
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
 */
 class Solution {
     public int[] twoSum(int[] nums, int target) {
